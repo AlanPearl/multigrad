15a30f91c03e84242c0ceb3eebe6a1b946830549
Alan Pearl <alanpearl13@gmail.com>
2024-07-15 19:49:27 -0500

    Update __init__.py

--- multigrad/__init__.py
+++ multigrad/__init__.py
@@ -1,7 +1,7 @@
 from ._version import __version__  # noqa
 
-from .multigrad import OnePointModel, OnePointGroup, util
-from .multigrad import reduce_sum, split_subcomms, split_subcomms_by_node
+from .multigrad import (OnePointModel, OnePointGroup, util, reduce_sum,
+                        split_subcomms, split_subcomms_by_node)
 
 __all__ = [
     "OnePointModel", "OnePointGroup", "reduce_sum",
